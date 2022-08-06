916a73e4b944f30a462d2b6cd32982dda4a2bf28
fernandoribeirosilva <fernando1996ribeirosilva@gmail.com>
2022-08-06 14:23:56 -0300

    hover e estados no TailwindCSS

--- estilizando-css-com-tailwindcss/src/App.tsx
+++ estilizando-css-com-tailwindcss/src/App.tsx
@@ -3,10 +3,14 @@ import { useState } from 'react'
 function App() {
 
   return (
-    <div className="flex flex-col w-ful bg-blue-900 text-white p-4 font-bold text-2xl text-center  md:flex-row md:items-center md:justify-between lg:text-black">
-      Texto qualquer aqui
+    <div className="bg-blue-300 p-5">
+      <input
+        type="text"
+        className="outline-none border border-transparent border-gray-300 p-2 rounded focus:ring-2 focus:ring-blue-700"
+        placeholder='Digite alguma coisa'
+      />
 
-      <button className="bg-green-300 text-black p-2 rounded-xl ml-2 mt-3 transition duration-300 hover:transition-all hover:bg-green-400 hover:cursor-pointer md:ml-3 md:mt-0 sx:w-2/4 sx:mx-auto sm:w-full md:w-1/3 md:mx-0">Clique aqui</button>
+      <button className="ml-2 p-3 bg-blue-400 rounded text-white font-bold transition background-color ease-linear duration-300 hover:bg-sky-700 hover:transition-all">Clique aqui</button>
     </div>
   )
 }
