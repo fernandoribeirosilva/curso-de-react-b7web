77cf6ee13c69ce4155924b949bfea6deeefa5b9c
fernandoribeirosilva <fernando1996ribeirosilva@gmail.com>
2022-08-17 13:57:12 -0300

    add descrição do useEffect

--- ciclo-de-vida-do-componente/src/App.tsx
+++ ciclo-de-vida-do-componente/src/App.tsx
@@ -1,5 +1,19 @@
 import { useEffect, useState } from 'react'
 
+/* 
+  1 - etapa do siclo de vida do componente (renderização)
+    quando um componente vai ser renderizando, primeiro ele é criado na memória,
+    isso é chamado de montagem do componente (primeiro siclo de vida do componente).
+
+  2 - etapa do siclo de vida do componente (atualização)
+    se ele tiver state e que vai sofrer alterações, ele vai ser atualizado.
+    então a cada atualização do componente, também atribuir a um siclo de vida a cada 
+    uma desta alterações.
+
+  3 - etapa do siclo de vida do componente (destruição)
+    quando o componente é removido da memória.
+*/
+
 function App() {
   const [name, setName] = useState<string>('Pedro');
 
@@ -12,7 +26,7 @@ function App() {
   }
 
   return (
-    <div className="flex flex-col items-center bg-slate-200">
+    <div className="flex flex-col items-center bg-slate-200 p-5">
       <h1 className='mx-5'>Hello <span className='underline'>{name}</span></h1>
       <button
         className="w-60 h-11 bg-slate-400 text-white p-2 mx-5 mt-3 rounded hover:bg-slate-500 ease-in-out duration-300 "
