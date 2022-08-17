9cf94e60b9a0f166fdc0f8cc0c6105a2d99dcdd3
fernandoribeirosilva <fernando1996ribeirosilva@gmail.com>
2022-08-17 15:42:02 -0300

    requisição simples

---
+++ ciclo-de-vida-do-componente/requisicao-get-simples/src/App.tsx
@@ -0,0 +1,45 @@
+import { useEffect, useState } from 'react'
+import { Movie } from './types/Movie';
+
+function App() {
+  const [movies, setMovies] = useState<Movie[]>([]);
+  const [loading, setLoading] = useState(false);
+
+
+  useEffect(() => {
+    fetch('https://api.b7web.com.br/cinema/')
+      .then(response => response.json())
+      .then(data => setMovies(data));
+  }, []);
+
+  useEffect(() => {
+    if (movies) setLoading(!loading);
+  }, [movies]);
+
+  return (
+    <div className='p-5'>
+      {loading ?
+        <div className='font-bold'>Carregando...</div>
+        :
+        <>
+          <div className='mb-5 font-bold underline underline-offset-4'>
+            Total de Filmes: {movies.length}
+          </div>
+
+          <div className="grid grid-cols-6 gap-3 ">
+            {movies.map((movie, index) => (
+              <div key={index}>
+                <img src={movie.avatar} alt="" className='w-36 block' />
+                <span>{movie.titulo}</span>
+              </div>
+            ))}
+          </div>
+        </>
+      }
+    </div>
+  )
+}
+
+export default App
+
+// https://api.b7web.com.br/cinema/
