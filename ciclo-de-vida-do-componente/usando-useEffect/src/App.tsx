d5fc50ec702aa222f99d350d5e9c124b8ef0bd4f
fernandoribeirosilva <fernando1996ribeirosilva@gmail.com>
2022-08-17 14:12:38 -0300

    eparando pasta

---
+++ ciclo-de-vida-do-componente/usando-useEffect/src/App.tsx
@@ -0,0 +1,45 @@
+import { useEffect, useState } from 'react'
+
+/* 
+  1 - etapa do siclo de vida do componente (renderização)
+    quando um componente vai ser renderizando, primeiro ele é criado na memória,
+    isso é chamado de montagem do componente (primeiro siclo de vida do componente).
+
+  2 - etapa do siclo de vida do componente (atualização)
+    se ele tiver state e que vai sofrer alterações, ele vai ser atualizado.
+    então a cada atualização do componente, também atribuir a um siclo de vida a cada 
+    uma desta alterações.
+
+  3 - etapa do siclo de vida do componente (destruição)
+    quando o componente é removido da memória.
+*/
+
+function App() {
+  const [name, setName] = useState<string>('Pedro');
+
+  // No useEffect, deve ser passado 2 parâmetros 
+  // 1 - função que será executada.
+  //  esta função deve ser executada quando o componente for renderizado ou criado.
+  //2 - array de dependências
+  //  estas dependência é a variável que será monitorada para saber se ela mudou o
+  //  valor, ele vai ser executado novamente.
+  useEffect(() => {
+    alert(`Olá`);
+  }, [name]);
+
+  const handleClick = () => {
+    setName('Fernando');
+  }
+
+  return (
+    <div className="flex flex-col items-center bg-slate-200 p-5">
+      <h1 className='mx-5'>Hello <span className='underline'>{name}</span></h1>
+      <button
+        className="w-60 h-11 bg-slate-400 text-white p-2 mx-5 mt-3 rounded hover:bg-slate-500 ease-in-out duration-300 "
+        onClick={handleClick}
+      >Mudar nome</button>
+    </div>
+  )
+}
+
+export default App
