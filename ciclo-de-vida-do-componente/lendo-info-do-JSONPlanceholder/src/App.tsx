543250872c41ebff06433863e753ac40aa391f4f
fernandoribeirosilva <fernando1996ribeirosilva@gmail.com>
2022-08-20 15:20:24 -0300

    feito requisição post

--- ciclo-de-vida-do-componente/lendo-info-do-JSONPlanceholder/src/App.tsx
+++ ciclo-de-vida-do-componente/lendo-info-do-JSONPlanceholder/src/App.tsx
@@ -1,11 +1,14 @@
 import { useEffect, useState } from 'react';
+import { Form } from './components/Form';
+import { Loading } from './components/Loading';
+import { Message } from './components/Message/Message';
+import { Post } from './components/Post';
 import { axiosInstance as axios } from './config/axios';
-import { Post } from './types/Post';
+import { Post as PostProps } from './types/Post';
 
 function App() {
   const [loading, setLoading] = useState(false);
-  const [posts, setPosts] = useState<Post[]>([]);
-  const [error, setError] = useState('');
+  const [posts, setPosts] = useState<PostProps[]>([]);
 
   useEffect(() => {
     loadMovies();
@@ -19,40 +22,34 @@ function App() {
       setLoading(false);
       setPosts(data);
     } catch (error: InstanceType<Error>) {
-      const message = error.response.data.error;
       console.log('Error => ', error);
       setLoading(false);
-      setError(message);
     }
   };
 
 
   return (
     <div className='p-5'>
-      {loading &&
-        <div className='font-bold'>Carregando...</div>
-      }
+      {loading && <Loading />}
+
+      <Form />
 
       {!loading && posts.length > 0 &&
         <>
-          <div className='mb-5 font-bold underline underline-offset-4'>
+          <div className='mb-2 font-bold underline underline-offset-4'>
             Total de Posts: {posts.length}
           </div>
 
           <div>
             {posts.map((item) => (
-              <div key={item.id} className="py-4">
-                <h4 className="font-bold">{item.title}</h4>
-                <small>#{item.id} - Usuário: {item.userId}</small>
-                <p>{item.body}</p>
-              </div>
+              <Post key={item.id} {...item} />
             ))}
           </div>
         </>
       }
 
       {!loading && posts.length === 0 &&
-        <div>Não tem post para mostra</div>
+        <Message message='Não tem post para mostra' />
       }
     </div>
   )
