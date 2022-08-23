68cd2656088e1684738000443c2c582158effd2c
fernandoribeirosilva <fernando1996ribeirosilva@gmail.com>
2022-08-23 15:25:45 -0300

    formatando o arquivo

--- ciclo-de-vida-do-componente/caso-real-de-uso-do-useReducer/src/reducers/peopleList.ts
+++ ciclo-de-vida-do-componente/caso-real-de-uso-do-useReducer/src/reducers/peopleList.ts
@@ -1,3 +1,4 @@
+import { useReducer } from "react";
 import { v4 as uuid4 } from "uuid";
 
 type Person = {
@@ -19,23 +20,32 @@ const reducer = (state: Person[], action: ReducerEstate) => {
   switch (action.type) {
     case "ADD":
       if (action.payload?.name) {
-        state.push({
+        const newState = [...state];
+
+        newState.push({
           id: uuid4(),
           name: action.payload?.name,
         });
+
+        return newState;
       }
+
       break;
     case "DEL":
       if (action.payload?.id) {
         state = state.filter((person) => person.id !== action.payload?.id);
       }
+
       break;
     case "ORDER":
       state = state.sort((previous, current) =>
         previous.name > current.name ? 1 : -1
       );
+
       break;
   }
   return state;
 };
-export const usePeopleList = () => {};
+export const usePeopleList = () => {
+  return useReducer(reducer, initialState);
+};
