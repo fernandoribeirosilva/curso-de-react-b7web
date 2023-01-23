2ab50ba2121d99ecb0a4d0459900dc925cadf254
fernandoribeirosilva <fernando1996ribeirosilva@gmail.com>
2023-01-23 15:00:22 -0300

    criando a primeira rotas

--- Rotas-com-React/src/App.tsx
+++ Rotas-com-React/src/App.tsx
@@ -1,8 +1,27 @@
-export function App() {
+import { Route, Routes } from "react-router-dom"
+import { About } from "./pages/About"
+import { Home } from "./pages/Home"
 
+
+export function App() {
   return (
-    <div>
-      ...
+    <div className="p-4">
+      <header>
+        <h1>Titulo do site</h1>
+      </header>
+      <hr />
+      <div className="py-4">
+
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/sobre" element={<About />} />
+        </Routes>
+
+      </div>
+      <hr />
+      <footer>
+        Todos os direitos reservados.
+      </footer>
     </div>
   )
 }
---
+++ Rotas-com-React/src/pages/Home.tsx
@@ -0,0 +1,7 @@
+export function Home() {
+  return (
+    <div>
+      Página Home
+    </div>
+  )
+}
