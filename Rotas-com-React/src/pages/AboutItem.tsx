62eb4430faba8f52b95187bbd3dc33b2943dd7b2
fernandoribeirosilva <fernando1996ribeirosilva@gmail.com>
2023-01-26 13:46:39 -0300

    redirecionamento de rotas

--- Rotas-com-React/src/pages/AboutItem.tsx
+++ Rotas-com-React/src/pages/AboutItem.tsx
@@ -1,11 +1,31 @@
-import { useParams } from "react-router-dom";
+import { useNavigate, useParams } from "react-router-dom";
 
 export function AboutItem() {
   const { slug } = useParams();
+  const navigate = useNavigate();
+
+  function handleBackButton() {
+    navigate(-1);
+  }
+
+  function handleHomeButton() {
+    const slug = 'ciclano';
+    navigate(`/sobre/${slug}`);
+  }
 
   return (
     <div>
       Página sobre {slug?.toUpperCase()} ({slug?.length} letras)
+      <hr />
+      <button
+        onClick={handleBackButton}
+      >
+        Voltar
+      </button>
+
+      <button onClick={handleHomeButton}>
+        Home
+      </button>
     </div>
   )
 }
