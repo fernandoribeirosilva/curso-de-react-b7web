5451afdcd2114eeaa7e608932fdfc0c4ac1519fd
fernandoribeirosilva <fernando1996ribeirosilva@gmail.com>
2023-01-27 14:55:29 -0300

    parâmetros de query

--- Rotas-com-React/src/pages/About.tsx
+++ Rotas-com-React/src/pages/About.tsx
@@ -1,8 +1,21 @@
-import { Link } from "react-router-dom";
+import { Link, useSearchParams } from "react-router-dom";
 
 export function About() {
+  const [searchParams, setSearchParams] = useSearchParams();
+
+  function setOrder(order: 'asc' | 'desc') {
+    searchParams.set('order', order);
+    setSearchParams(searchParams);
+  }
+
   return (
     <div>
+      Filtro: {searchParams.get('filter')}<br />
+      Ordem: {searchParams.get('order')}
+      <hr />
+      <button onClick={() => setOrder('asc')}>Asc</button>
+      <button onClick={() => setOrder('desc')}>Desc</button>
+      <hr />
       Página Sobre:
       <ul>
         <li>
@@ -12,6 +25,8 @@ export function About() {
           <Link to="/sobre/pedro">Pedro</Link>
         </li>
       </ul>
+
+
     </div>
   )
 }
